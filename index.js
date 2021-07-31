60f826315b6aae396acb6cca97b999c4612415ad
FranciG <beodo0@gmail.com>
2021-07-31 16:51:45 +0200

    3.4 delete

--- index.js
+++ index.js
@@ -48,6 +48,14 @@ app.get('/info', (req, res) => {
     res.json(`The phonebook has info for ${persons.length} people. Request time: ${Date().toLocaleString()}`)
   })
 
+ //delete
+ 
+ app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+  })
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
